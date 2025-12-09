193e00d595bbcd513c0387ab44579f7856ea1986
SneaX-23 <sudeepmagdum321@gmail.com>
2025-12-09 21:32:25 +0530

    Add retry loop to email cinsumer

--- microservices-project/email-service/src/consumers/payment.consumer.ts
+++ microservices-project/email-service/src/consumers/payment.consumer.ts
@@ -7,50 +7,54 @@ const consumer = createKafkaConsumer();
 
 // Kafka conumer for payments, sends emails on payment events
 export const startEamilPaymentConsumer = async () => {
-    try {
-        await consumer.connect();
+    while (true) {
+        try {
+            await consumer.connect();
 
-        // Subscribe to topic
-        await consumer.subscribe({topic: topics.PAYMENT_EVENTS, fromBeginning: true});
-        await consumer.subscribe({ topic: topics.USER_CREATED, fromBeginning: true });
+            // Subscribe to topic
+            await consumer.subscribe({topic: topics.PAYMENT_EVENTS, fromBeginning: true});
+            await consumer.subscribe({ topic: topics.USER_CREATED, fromBeginning: true });
 
-        await consumer.run({
-            eachMessage: async ({topic, partition, message}) => {
-                if(!message.value) return;
+            await consumer.run({
+                eachMessage: async ({topic, partition, message}) => {
+                    if(!message.value) return;
 
-                const event = JSON.parse(message.value.toString());
+                    const event = JSON.parse(message.value.toString());
 
-               try {
-                    let userEmail: string;
+                try {
+                        let userEmail: string;
 
-                    //  Handle "new_user" events, Email is already in payload
-                    if (event.type === "new_user") {
-                        userEmail = event.data.email;
-                    } 
-                    // Handle Payment events, Need to fetch email using userId
-                    else {
-                        if (!event.data.userId) {
-                            logger.warn(`Event ${event.type} missing userId. Skipping.`);
-                            return; 
+                        //  Handle "new_user" events, Email is already in payload
+                        if (event.type === "new_user") {
+                            userEmail = event.data.email;
+                        } 
+                        // Handle Payment events, Need to fetch email using userId
+                        else {
+                            if (!event.data.userId) {
+                                logger.warn(`Event ${event.type} missing userId. Skipping.`);
+                                return; 
+                            }
+                            userEmail = await getUserEmail(event.data.userId);
                         }
-                        userEmail = await getUserEmail(event.data.userId);
-                    }
 
-                    // Send the email
-                    if (userEmail) {
-                        await sendMail(userEmail, event.type, event.data);
-                        logger.info(`Email sent to: ${userEmail} for event: ${event.type}`);
-                    } else {
-                        logger.error(`Failed to resolve email for event: ${event.type}`);
-                    }
+                        // Send the email
+                        if (userEmail) {
+                            await sendMail(userEmail, event.type, event.data);
+                            logger.info(`Email sent to: ${userEmail} for event: ${event.type}`);
+                        } else {
+                            logger.error(`Failed to resolve email for event: ${event.type}`);
+                        }
 
-                } catch (err) {
-                    logger.error({ message: `Error processing message: ${event.type}`, error: err });
+                    } catch (err) {
+                        logger.error({ message: `Error processing message: ${event.type}`, error: err });
+                    }
                 }
-            }
-        })
-    } catch (err) {
-        logger.error({message: `Failed to start payment consumer.`, error: err});
-        logger.info("Consumer crashed — will retry.");
+            });
+            break;
+        } catch (err) {
+            logger.error({message: `Failed to start payment consumer.`, error: err});
+            logger.info("Consumer crashed — will retry.");
+            await new Promise((resolve) => setTimeout(resolve, 5000));
+        }
     }
-}
+};
--- microservices-project/email-service/src/index.ts
+++ microservices-project/email-service/src/index.ts
@@ -1,18 +1,22 @@
+import express from "express";
 import { startEamilPaymentConsumer } from "./consumers/payment.consumer";
 import logger from "./utils/logger";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Health check 
+app.get("/health", (req, res) => res.send("Email Service is running"));
+
 async function main() {
   logger.info("Email Service starting...");
+  
+  startEamilPaymentConsumer();
 
-  try {
-    await startEamilPaymentConsumer();
-    logger.info("Kafka consumer running...");
-  } catch (err) {
-      logger.error({
-        message: "Failed to start email service",
-        error: err
-    });
-  }
+  // Start HTTP Server
+  app.listen(PORT, () => {
+      logger.info(`Email Service listening on port ${PORT}`);
+  });
 }
 
 main();
